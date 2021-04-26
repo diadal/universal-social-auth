4c2a06c4a27c2455f98052defd7877f8f62f5b61
diadal <diadal2014@gmail.com>
2021-04-26 08:23:29 +0100

    Update popup.ts
    catch errors

--- src/oauth/popup.ts
+++ src/oauth/popup.ts
@@ -1,99 +1,102 @@
-
-import { parseQueryString, getFullUrlPath, isUndefined } from '../utils'
+import { parseQueryString, getFullUrlPath, isUndefined } from '../utils';
 
 export default class OAuthPopup {
-  popupOptions: Record<string, unknown>
-  popup: Window | null
-  url: string
-  name: string
-  constructor (url: string, name: string, popupOptions: Record<string, unknown>) {
-    this.popup = null
-    this.url = url
-    this.name = name
-    this.popupOptions = popupOptions
+  popupOptions: Record<string, unknown>;
+  popup: Window | null;
+  url: string;
+  name: string;
+  constructor(
+    url: string,
+    name: string,
+    popupOptions: Record<string, unknown>
+  ) {
+    this.popup = null;
+    this.url = url;
+    this.name = name;
+    this.popupOptions = popupOptions;
   }
 
-  open (redirectUri: string, skipPooling: boolean) {
+  open(redirectUri: string, skipPooling: boolean) {
     try {
-      this.popup = window.open(this.url, this.name, this._stringifyOptions())
+      this.popup = window.open(this.url, this.name, this._stringifyOptions());
       if (this.popup && this.popup.focus) {
-        this.popup.focus()
+        this.popup.focus();
       }
 
       if (skipPooling) {
-        return Promise.resolve()
+        return Promise.resolve();
       } else {
-        const pool = this.pooling(redirectUri)
-        // console.log('pool', pool)
-        return pool
+        const pool = this.pooling(redirectUri);
+        return pool;
       }
     } catch (e) {
-      return Promise.reject(new Error('OAuth popup error occurred'))
+      return Promise.reject('OAuth popup error occurred');
     }
   }
-  
-  pooling (redirectUri: string) {
-    return new Promise((resolve:(value: unknown) => void) => {
-      const redirectUriParser = document.createElement('a')
-      redirectUriParser.href = redirectUri
-      const redirectUriPath = getFullUrlPath(redirectUriParser)
+
+  pooling(redirectUri: string) {
+    return new Promise((resolve: (value: unknown) => void, reject) => {
+      const redirectUriParser = document.createElement('a');
+      redirectUriParser.href = redirectUri;
+      const redirectUriPath = getFullUrlPath(redirectUriParser);
 
       let poolingInterval = window.setInterval(() => {
-        if (!this.popup || this.popup.closed || this.popup.closed === undefined) {
-          clearInterval(poolingInterval)
-          poolingInterval = 0
-          reject(new Error('Auth popup window closed'))
+        if (
+          !this.popup ||
+          this.popup.closed ||
+          this.popup.closed === undefined
+        ) {
+          clearInterval(poolingInterval);
+          poolingInterval = 0;
+          reject('Auth popup window closed');
         }
 
         try {
-          const location = <Window>this.popup
+          const location = <Window>this.popup;
 
-          const popupWindowPath = getFullUrlPath(location.location)
-          // console.log('location.location', location.location)
-
-          // console.log('redirectUriPath', redirectUriPath)
-
+          const popupWindowPath = getFullUrlPath(location.location);
           if (popupWindowPath === redirectUriPath) {
             if (location.location.search || location.location.hash) {
-              const query = parseQueryString(location.location.search.substring(1).replace(/\/$/, ''))
-              const hash = parseQueryString(location.location.hash.substring(1).replace(/[/$]/, ''))
+              const query = parseQueryString(
+                location.location.search.substring(1).replace(/\/$/, '')
+              );
+              const hash = parseQueryString(
+                location.location.hash.substring(1).replace(/[/$]/, '')
+              );
               const params = {
                 ...query,
                 ...hash
-              }
+              };
               if (params.error) {
-                reject(new Error((<string>params.error)))
+                reject(<string>params.error);
               } else {
-                resolve(params)
+                resolve(params);
               }
             } else {
-              reject(new Error('OAuth redirect has occurred but no query or hash parameters were found.'))
+              reject(
+                'OAuth redirect has occurred but no query or hash parameters were found.'
+              );
             }
 
-            clearInterval(poolingInterval)
-            poolingInterval = 0
-            location.close()
+            clearInterval(poolingInterval);
+            poolingInterval = 0;
+            location.close();
           }
         } catch (e) {
           // Ignore DOMException: Blocked a frame with origin from accessing a cross-origin frame.
         }
-      }, 250)
-    })
+      }, 250);
+    });
   }
-  
-  _stringifyOptions () {
-    const options:string[] = []
-    const Popup = this.popupOptions
-    // console.log('Popup', Popup)
+
+  _stringifyOptions() {
+    const options: string[] = [];
+    const Popup = this.popupOptions;
     for (const optionKey in Popup) {
-      if (!isUndefined(<undefined>(Popup[optionKey]))) {
-        options.push(`${optionKey}=${<string>(Popup[optionKey])}`)
+      if (!isUndefined(<undefined>Popup[optionKey])) {
+        options.push(`${optionKey}=${<string>Popup[optionKey]}`);
       }
     }
-    return options.join(',')
+    return options.join(',');
   }
-}
-
-function reject (arg0: Error) {
-  throw new Error(`Function not implemented. ${<string><unknown>arg0}`)
 }
